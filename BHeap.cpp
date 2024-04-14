2555d76569ed90ac8103d372574f8912d71497d8
wwmink <wwmink@crimson.ua.edu>
2024-04-14 11:48:49 -0500

    Implemented insert, peekKey, refactored insertion constructor and added function stubs.

--- BHeap.cpp
+++ BHeap.cpp
@@ -5,12 +5,24 @@
 #include "HeapNode.cpp"
 using namespace std;
 
+/**
+ * @brief Class that represents a Binomial Heap
+ * 
+ * @tparam KeyType Type of the key of the Binomial Heap
+ */
 template <typename KeyType> class BHeap {
     private:
         HeapNode<KeyType> *rootNode;
         int size;
         int maxDegree;
-    
+
+        /**
+         * @brief Consolidates the heap by merging trees of the same degree
+         */
+        void consolidate() {
+            // IMPLEMENT THIS
+        }
+
     public:
         /**
          * @brief Construct a new BHeap with a doubly linked root list
@@ -25,16 +37,7 @@ template <typename KeyType> class BHeap {
 
             // Insert all keys into the heap
             for (int i = 0; i < s; i++) {
-                // First insertion into the heap
-                if (rootNode == NULL) {
-                    rootNode = new HeapNode<KeyType>(K[i]);
-                } else {
-                    HeapNode<KeyType> *newNode = new HeapNode<KeyType>(K[i]);
-                    rootNode->swapSibling(newNode, true);
-                    if (newNode->getKey() < rootNode->getKey()) {
-                        rootNode = newNode;
-                    }
-                }
+                insert(K[i], true);
             }
         }
 
@@ -45,5 +48,53 @@ template <typename KeyType> class BHeap {
             rootNode = NULL;
             size = 0;
             maxDegree = 0;
+        }
+
+        /**
+         * @brief Inserts a key into the heap's root list
+         * 
+         * @param k Key to insert
+         * @param constructorCall Used to determine if the size should be incremented
+         */
+        void insert(KeyType k, bool constructorCall) {
+            HeapNode<KeyType> *newNode = new HeapNode<KeyType>(k);
+            if (rootNode == NULL) {
+                rootNode = newNode;
+            } else {
+                rootNode->swapSibling(newNode, true);
+                if (newNode->getKey() < rootNode->getKey()) {
+                    rootNode = newNode;
+                }
+            }
+            if (!constructorCall) size++;
+        }
+
+        /**
+         * @brief Extracts the minimum key from the heap and consolidates the heap
+         * 
+         * @return KeyType Minimum key in the heap
+         */
+        KeyType extractMin() {
+            // IMPLEMENT THIS
+        }
+
+        void merge(BHeap<KeyType> &H) {
+            // IMPLEMENT THIS
+        }
+
+        void printKey() {
+            // IMPLEMENT THIS
+        }
+
+        /**
+         * @brief Returns the minimum key in the heap
+         * 
+         * @return KeyType Minimum key in the heap
+         */
+        KeyType peekKey() {
+            if (rootNode == NULL) {
+                throw runtime_error("Heap is empty");
+            }
+            return rootNode->getKey();
         }
 };
